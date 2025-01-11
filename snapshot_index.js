b7f9e9dc72f748bbeb4f30e2d66acaa68fef405e
samara1531 <samara1531@gmail.com>
2025-01-12 00:18:45 +0300

    Update snapshot_index.js

--- snapshot_index.js
+++ snapshot_index.js
@@ -85,8 +85,8 @@ async function getDetails(target, subtarget) {
 
     if (kmodsLinks.length >= 0) {
       // Берем шестую ссылку из найденных
-      const sixthKmodLink = kmodsLinks[0]; // Индексация с 0, поэтому седьмой элемент — это kmodsLinks[6]
-      const sixthKmodUrl = `${kmodsUrl}${sixthKmodLink}index.json`; // Переход по шестой ссылке и получаем index.json
+      const sixthKmodLink = kmodsLinks[0]; // Индексация с 0, поэтому 1 элемент — это kmodsLinks[0]
+      const sixthKmodUrl = `${kmodsUrl}${sixthKmodLink}index.json`; // Переход по ссылке и получаем index.json
 
       console.log(`Fetching index.json from: ${sixthKmodUrl}`); // Логируем URL для index.json
 
